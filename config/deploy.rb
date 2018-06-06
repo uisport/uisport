2fb2d728bade2acba4e46cb3da41f29b1bc2474a
Hemant Gupta <hemant.yuvasoft@gmail.com>
2018-06-06 16:59:08 +0530

    change deploy path

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,8 +16,8 @@ set :use_sudo, false
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :tmp_dir, "/home/uisport"
-set :deploy_to, "/home/uisport"
+set :tmp_dir, "/var/www/uisport.com"
+set :deploy_to, "/var/www/uisport.com"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
